f208ea6e387f902e23c1bd821ecd3a806c11f390
Berk Unsal <mberkunsall@gmail.com>
2022-07-28 13:50:24 +0300

    Background effect & DOTween implemented

--- Assets/Scripts/Traps/SawMovement.cs
+++ Assets/Scripts/Traps/SawMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class SawMovement : MonoBehaviour
 {
@@ -13,8 +14,21 @@ public class SawMovement : MonoBehaviour
     [SerializeField]
     private float turnSpeed;
 
+    [SerializeField]
+    private float cyleLength = 2;
+
     private bool reached = false;
 
+    private void Start()
+    {
+        transform.DOMove(destinationPoint.position, cyleLength).SetEase(Ease.InOutQuad).SetLoops(-1,LoopType.Yoyo);
+        transform.DORotate(new Vector3(0, 0, 360),cyleLength*0.5f,RotateMode.FastBeyond360).SetLoops(-1,LoopType.Restart).SetEase(Ease.Linear);
+    }
+
+
+
+    //Old version
+    /* 
     // Start is called before the first frame update
     void Start()
     {
@@ -43,4 +57,5 @@ public class SawMovement : MonoBehaviour
         }
         transform.Rotate(0, 0, turnSpeed*50 * Time.deltaTime);
     }
+    */
 }
